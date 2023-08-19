6f7576e6f6d2aca3e7efb7367f513f1d62105527
Rayan <itsrayyan0@gmail.com>
2023-08-19 11:42:21 -0700

    fixed6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - alphabets
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
