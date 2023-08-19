d5e87f43f1823655cd42f6c61fdccb6c48db1c6e
Rayan <itsrayyan0@gmail.com>
2023-08-19 11:36:31 -0700

    fixed5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - alphabets
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
+}
