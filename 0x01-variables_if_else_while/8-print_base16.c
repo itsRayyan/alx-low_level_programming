793bf2801382d1536d6cd47069946ec825e0ba6f
Rayan <itsrayyan0@gmail.com>
2023-08-19 11:49:18 -0700

    fixed8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - alphabets
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
